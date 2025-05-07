54275e437545dc3045e22071c74ea88b652bab88
Jonah <jojohawkes414@gmail.com>
2025-05-07 01:33:54 -0700

    Add files via upload

---
+++ +1.lua
@@ -0,0 +1,11 @@
+print("type a number to add 1 to it")
+local a = io.read("*n")
+local b = 1
+local sum = a + b
+print(a.. "+1 is " .. sum)
+print("type a number to add 2 to it")
+local a = io.read("*n")
+local b = -2
+local sum = a + b
+print(a.. "+2 is " .. sum)
+print("get fucked idiot")
